9c9bd38f17b9ffd5d2719ee9fecc84ad34103c40
Juancito <cjuan.chona@hotmail.com>
2022-06-17 02:27:32 -0300

    la logica del router esta feisima pero funciona

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 import home from './pages/home.js'
+import reproductor from './pages/reproductor.js'
 const app = document.getElementById('app')
 
+const appReset = (app,callback,id='')=>{
+    app.innerHTML=''
+    app.appendChild(callback(id))
+}
+
 const routes = {
-    '':()=>app.appendChild(home()),
-    '404':()=>app.innerHTML='<h1>404<h1/>'
+    '':()=>appReset(app,home),
+    '404':()=>app.innerHTML='<h1>404<h1/>',
+    'reproductor':(id)=>appReset(app,reproductor,id)
 }
 
 function router(e){
     e.preventDefault()
+
+    // corregir esta logica por dio
+    const decicion = resetHashUrl(window.location.hash)
+    if(decicion.success) return routes[decicion.path](decicion.id)
+    // corregir esta logica por dio
+
     routes[window.location.hash]? routes[window.location.hash]() : routes['404']()
 }
 
-// function resetHashUrl(string)
+function resetHashUrl(string){
+    const array = string.split('/')
+    // console.log(array)
+    if(array[1]=='reproductor') return{
+        success:true,
+        path:array[1],
+        id:array[2]
+    }
+    return{
+
+    }
+}
 
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
--- pages/reproductor.js
+++ pages/reproductor.js
@@ -1,3 +1,5 @@
-export default function reproductor(){
-    
+export default function reproductor(id){
+    const idp = document.createElement('p')
+    idp.textContent = id
+    return idp
 }
